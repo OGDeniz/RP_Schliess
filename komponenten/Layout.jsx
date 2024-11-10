e84128af6a059a5b066de5546f2b272e192b88ec
OGDeniz <118958904+OGDeniz@users.noreply.github.com>
2024-11-10 06:39:36 +0100

    Überflüssige Importe aus Layout.jsx entfernt

--- komponenten/Layout.jsx
+++ komponenten/Layout.jsx
@@ -1,9 +1,6 @@
 import Navigation from "@/komponenten/Navigation";
 import FussZeile from "@/komponenten/FussZeile";
 import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
 
 
 export default function Layout({ children }) {
