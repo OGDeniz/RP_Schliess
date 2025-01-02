1b5c83f3c7a1c7d6c3e568a40bb7dafae471f76f
OGDeniz <118958904+OGDeniz@users.noreply.github.com>
2025-01-02 15:53:16 +0100

    Aktualisiere next.config.js: Aktiviere reactStrictMode, füge Bildoptimierungen hinzu und konfiguriere Umgebungsvariablen. Ergänze critters zur Verbesserung der Ladezeiten.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-      wasm: false, // Deaktiviert WebAssembly, um Speicherprobleme zu umgehen
       optimizeCss: true, // Reduziert die CSS-Verarbeitung
     },
     // Optimierungen zur Reduzierung der Serverlast
     compress: true, // Aktiviert die GZIP-Kompression für schnelleres Laden
     poweredByHeader: false, // Entfernt das 'X-Powered-By'-Header für Sicherheit
-    reactStrictMode: false, // Verhindert doppelte Renderings in Entwicklungsumgebungen
-    swcMinify: true, // Aktiviert die SWC-Minifizierung für kleinere JavaScript-Dateien
-    // Limits für Bilder setzen, um Speicherbedarf zu verringern
-    images: {
-      formats: ['image/avif', 'image/webp'], // Optimierte Bildformate
-      minimumCacheTTL: 60, // Bild-Cache auf 60 Sekunden setzen
-      deviceSizes: [640, 750, 828, 1080, 1200], // Definiert unterstützte Bildgrößen
-    },
+    reactStrictMode: true, // Verhindert doppelte Renderings in Entwicklungsumgebungen
+    
+images: {
+  formats: ['image/avif', 'image/webp'], // Optimierte Bildformate
+  deviceSizes: [640, 750, 828, 1080, 1200], // Definiert unterstützte Bildgrößen
+},
+
     // Anpassungen für Routen und API
     async headers() {
       return [
@@ -26,9 +24,10 @@ const nextConfig = {
         },
       ];
     },
+    
     // Konfiguration von Umgebungsvariablen
     env: {
-      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.example.com',
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'https://api.example.com',
     },
     // Deaktiviert große Server-Funktionen, die Speicher verbrauchen
     output: 'standalone', // Für eine kleinere Build-Größe optimiert
