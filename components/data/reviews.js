94d00bc3cdb91f4b32e04c0614a445786b020e36
OGDeniz <118958904+OGDeniz@users.noreply.github.com>
2025-01-14 21:12:34 +0100

    Füge Bewertungen hinzu: Implementiere eine neue Datei für Kundenbewertungen mit Beispieltexten, Autoren und Bewertungen.

---
+++ components/data/reviews.js
@@ -0,0 +1,15 @@
+﻿const reviews = [
+    {
+        text: "Sehr schneller und professioneller Service!",
+        author: "Max Mustermann",
+        rating: 5,
+        date: "15.03.2024",
+    },
+    {
+        text: "Top Service, kann ich nur weiterempfehlen!",
+        author: "Erika Musterfrau",
+        rating: 5,
+        date: "22.02.2024",
+    },
+];
+export default reviews;
